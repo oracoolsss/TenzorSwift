c7a9a8cff2b6862a15adb1d41010dd4021284fec
oracool <>
2019-11-03 10:37:44 -0800

    code was cleaned according to the swift code style

--- cars2/cars2/Storage.swift
+++ cars2/cars2/Storage.swift
@@ -1,66 +1,58 @@
-//
-//  Storage.swift
-//  AutoCatalog
-//
-//  Created by Гость on 24/10/2019.
-//  Copyright © 2019 sia. All rights reserved.
-//
 import Foundation
 
 private let fileURL = URL(fileURLWithPath: "./cars.txt")
 
 class Storage {
-    internal private(set) var cars: [Car] = []
-    
-    func addCar(_ car: Car) {
-        cars.append(car)
+  internal private(set) var cars: [Car] = []
+  
+  func addCar(_ car: Car) {
+    cars.append(car)
+  }
+  
+  func addCar(_ car: Car,_ ind: Int) {
+    if ind >= 0 {
+      cars.insert(car, at: min(ind, cars.count))
+    }
+    else {
+      print("wrong index")
+    }
+  }
+  
+  func removeCar(_ carForRemove: Car) {
+    cars.removeAll { car in
+      return car == carForRemove
+    }
+  }
+  
+  func removeCar(_ index: Int) {
+    cars.remove(at: index)
+  }
+  
+  func removeCar(_ param: String) {
+    cars.removeAll { car in
+      return car.name.contains(param) || car.model.contains(param) || String(car.year).contains(param)
+    }
+  }
+  
+  func save() {
+    guard let data = try? JSONEncoder().encode(cars) else {
+      fatalError("Can't encode data")
+    }
+    try? data.write(to: fileURL)
+  }
+  
+  func load() {
+    guard let data = try? Data(contentsOf: fileURL) else {
+      print("there is no any data in folder")
+      return
     }
     
-    func addCar(_ car: Car,_ ind: Int) {
-        if ind >= 0 {
-            cars.insert(car, at: min(ind, cars.count))
-        }
-        else {
-            print("wrong index")
-        }
+    guard let carsData = try? JSONDecoder().decode([Car].self, from: data) else {
+      print("wrong data")
+      return
     }
-    
-    func removeCar(_ carForRemove: Car) {
-        cars.removeAll { car in
-            return car == carForRemove
-        }
-    }
-    
-    func removeCar(_ index: Int) {
-        cars.remove(at: index)
-    }
-    
-    func removeCar(_ param: String) {
-        cars.removeAll { car in
-            return car.name.contains(param) || car.model.contains(param) || String(car.year).contains(param)
-        }
-    }
-    
-    func save() {
-        guard let data = try? JSONEncoder().encode(cars) else {
-            fatalError("Can't encode data")
-        }
-        try? data.write(to: fileURL)
-    }
-    
-    func load() {
-        guard let data = try? Data(contentsOf: fileURL) else {
-            print("there is no any data in folder")
-            return
-        }
-        
-        guard let carsData = try? JSONDecoder().decode([Car].self, from: data) else {
-            print("wrong data")
-            return
-        }
-        cars = carsData
-        
-    }
-    
-    
+    cars = carsData
+  }
+  
+  
 }
--- cars2/cars2/main.swift
+++ cars2/cars2/main.swift
@@ -1,10 +1,3 @@
-//
-//  main.swift
-//  AutoCatalog
-//
-//  Created by Ивлев А.Е. on 22/10/2019.
-//  Copyright © 2019 Ивлев А.Е. All rights reserved.
-//
 let storage = Storage()
 storage.load()
 
