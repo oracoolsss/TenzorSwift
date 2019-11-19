41e427f6ada27985c20cd017fea2bacd443de0d5
oracool <>
2019-11-19 02:30:16 -0800

    added task3 realisation

--- UIKietTest2/LectionUIKitTest/ImageListViewController.swift
+++ UIKietTest2/LectionUIKitTest/ImageListViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+//import Foundation
 
 private struct CellData {
-  let image: UIImage?
+  //let image: UIImage?
   let text: String
+  let imageSubscription: ImageSubscription
 }
 
 private struct SectionData {
@@ -19,8 +21,23 @@ private struct SectionData {
   let cells: [CellData]
 }
 
+typealias ImageSetter = ((UIImage, Int) -> Void)
+
+class ImageSubscription {
+  private var imageSetter: ImageSetter? = nil
+  
+  func subscribe (_ closure: @escaping ImageSetter) {
+    imageSetter = closure
+  }
+  
+  func set(_ image: UIImage, status: Int) {
+    imageSetter?(image, status)
+  }
+}
+
 class ImageListViewController: UIViewController {
   
+  /*
   @IBOutlet var tableView: UITableView!
   
   private let cheImage = UIImage(named: "cheboxary.jpg")
@@ -43,19 +60,80 @@ class ImageListViewController: UIViewController {
       CellData(image: cheImage, text: "World")
       ])
   ]
+ */
+  @IBOutlet var tableView: UITableView!
+  
+  @IBOutlet weak var imageView: UIImageView!
+  
+  private var sections = [SectionData]()
+  
+  private var urlSession = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+  
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    imageView.isHidden = true
+    let gesture = UITapGestureRecognizer(target: self, action: #selector(oneTap(sender:)))
+
+    imageView.addGestureRecognizer(gesture)
+    
     tableView.delegate = self
     tableView.dataSource = self
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellIdentifier")
     
     view.backgroundColor = .lightGray
     tableView.backgroundColor = .gray
     
     tableView.tableFooterView = UIView(frame: .zero)
+    
+    let kittySubscription = ImageSubscription()
+    let roachSubscription = ImageSubscription()
+    var animalSubscriptions = [ImageSubscription]()
+    animalSubscriptions.append(kittySubscription)
+    animalSubscriptions.append(roachSubscription)
+    
+    let animalCellData = [
+      CellData(text: "kitty", imageSubscription: animalSubscriptions[0]),
+      CellData(text: "roach)))", imageSubscription: animalSubscriptions[1])
+    ]
+    
+    sections.append(SectionData(headerText: "Animals", footerText: "", cells: animalCellData))
+    //sections.append(SectionData(headerText: "Faces", footerText: "", cells: facesCellData))
+    
+    let kittyUrl = URL(string: "https://sun9-55.userapi.com/c852232/v852232181/bc055/GumFgzYECK0.jpg")!
+    let roachUrl = URL(string: "https://i.kym-cdn.com/entries/icons/original/000/030/917/cover1.jpg")!
+    
+    let task = urlSession.dataTask(with: kittyUrl) { (data, response, error) in
+      guard let imageData = data, let image = UIImage(data: imageData) else {
+        return
+      }
+      
+      let httpResponse = response as! HTTPURLResponse
+      
+      animalSubscriptions[0].set(image, status: httpResponse.statusCode)
+    }
+    task.resume()
+    
+    let task2 = urlSession.dataTask(with: roachUrl) { (data, response, error) in
+      guard let imageData = data, let image = UIImage(data: imageData) else {
+        return
+      }
+      
+      let httpResponse = response as! HTTPURLResponse
+      
+      animalSubscriptions[1].set(image, status: httpResponse.statusCode)
+    }
+    task2.resume()
+  }
+  
+  @objc func oneTap(sender: UIGestureRecognizer) {
+    imageView.isHidden = true
+    tableView.isHidden = false
   }
 }
+
+
 
 extension ImageListViewController: UITableViewDelegate, UITableViewDataSource {
   func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -83,6 +161,26 @@ extension ImageListViewController: UITableViewDelegate, UITableViewDataSource {
     // indexPath.section
     
     return TableCell()
+  }
+  
+  func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+    let data = sections[indexPath.section].cells[indexPath.row]
+    data.imageSubscription.subscribe {(image, status)  in
+      self.imageView.image = image
+    }
+    print("\(data.text)")
+    //let cheImage = UIImage(named:"cheboxary.jpg")
+    //mageView.image = cheImage
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    tableView.isHidden = true
+    imageView.isHidden = false
+    //print("\(data.imageSubscription)")
+
+  }
+  
+  @IBAction private func imageOnClick(_ sender: Any) {
+    imageView.isHidden = true
+    tableView.isHidden = false
   }
   
   func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
@@ -130,6 +228,10 @@ private class TableCell: UITableViewCell {
   
   func configure(data: CellData) {
     imageNameLabel.text = data.text
-    previewImageView.image = data.image
+    //previewImageView.image = data.image
+    data.imageSubscription.subscribe {[weak self] (image, status)  in
+      self?.previewImageView.image = image
+    }
   }
+  
 }
